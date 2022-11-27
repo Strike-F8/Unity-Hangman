550de8d37bb4bc071673ae70890415919f6ef82f
Edward Fernandez <edward.fernandez@pgip-tech.com>
2022-11-27 13:24:48 -0500

    Initial commit First commit for hangman game. This is a Unity project that I plan to build for a webpage. A 2D hangman game that draws random words from a database.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private string[] words;
+
+    private int score;
+    private int lives;
+    private int level;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initialize the game
+        // Retrieve words into list
+
+    }
+}
