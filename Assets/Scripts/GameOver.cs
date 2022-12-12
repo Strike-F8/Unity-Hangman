05365b9052e60c439f9dc771e3e1c4bb1eb841a7
Edward Fernandez <edward.fernandez@pgip-tech.com>
2022-12-12 12:18:51 -0500

    Display the word on the game over screen Added a text display to show the player the word on the game over screen

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,10 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameOver : MonoBehaviour
 {
+    private string Word;
+    public TextMeshProUGUI WordDisplay;
+
+    public void Awake()
+    {
+        Word = FindObjectOfType<GameManager>().word.ArrayToString();
+        WordDisplay.text = $"The word was {Word}";
+    }
     public void MainMenu()
     {
         SceneManager.LoadScene("MainMenu");
